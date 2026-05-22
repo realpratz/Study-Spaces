d0dab2b50a74c68d53ec9981cc1a8dbb33fbbcae
Pratyush Priyadarshi <pratyushpriyadarshiofficial@gmail.com>
2026-05-22 23:49:19 +0530

    feat: created data model and json parse handling

---
+++ lib/models/study_space.dart
@@ -0,0 +1,34 @@
+class StudySpace{
+  final String id; 
+  final DateTime createdAt;
+  final String name;
+  final String? description;
+  final bool isPublic;
+  final String inviteCode;
+  final String? imageUrl;
+  final String creatorId;
+
+  StudySpace({
+    required this.id,
+    required this.createdAt,
+    required this.name,
+    this.description,
+    required this.isPublic,
+    required this.inviteCode,
+    this.imageUrl,
+    required this.creatorId,
+  });
+
+  factory StudySpace.fromJSON(Map<String,dynamic> json) {
+    return StudySpace(
+      id: json['id'] as String,
+      createdAt: DateTime.parse(json['created_at']),
+      name: json['name'] as String,
+      description: json['description'] as String?,
+      isPublic: json['is_public'] as bool,
+      inviteCode: json['invite_code'] as String,
+      imageUrl: json['image_url'] as String?,
+      creatorId: json['creator_id'] as String,
+    );
+  }
+}
