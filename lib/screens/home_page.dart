52bc347bcd8fe1314b2db321b23a96555c4d30fe
Pratyush Priyadarshi <pratyushpriyadarshiofficial@gmail.com>
2026-05-23 14:43:18 +0530

    feat: added ui for creating space

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -30,6 +30,76 @@ class _HomePageState extends State<HomePage> {
           child: Text('Sign Out'),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){
+          showModalBottomSheet(
+              context: context, 
+              isScrollControlled: true,
+              builder: (context) => const CreateSpaceSheet(),
+            );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CreateSpaceSheet extends StatefulWidget {
+  const CreateSpaceSheet({super.key});
+
+  @override
+  State<CreateSpaceSheet> createState() => _CreateSpaceSheetState();
+}
+
+class _CreateSpaceSheetState extends State<CreateSpaceSheet> {
+  final TextEditingController _spaceName = TextEditingController();
+  final TextEditingController _description = TextEditingController();
+  bool isPublic = false;
+
+  @override
+  void dispose(){
+    _spaceName.dispose();
+    _description.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Space Name'),
+                ),
+                controller: _spaceName,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Description'),
+                ),
+                controller: _description,
+              ),
+              SwitchListTile(
+                title: Text('Others can view this space'),
+                value: isPublic,
+                onChanged: (bool value) {
+                  setState(() {
+                    isPublic=value;
+                  });
+                }
+              ),
+              ElevatedButton(
+                onPressed: (){}, 
+                child: Text('Create Space'),
+              ),
+            ]
+        )
+      ),
     );
   }
 }
